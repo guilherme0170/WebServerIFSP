341c6d21b4acc34eaee4a98991d0f03cd13352b9
Lucas Sousa <lucas.sousa@2rpnet.com>
2020-09-29 16:06:08 -0300

    cadastro e lista de projetos

--- app.py
+++ app.py
@@ -7,34 +7,35 @@ app = Flask(__name__)
 # Configure db
 db = yaml.load(open('db.yaml'))
 app.config['MYSQL_HOST'] = db['mysql_host']
-app.config['MYSQL_PORT'] = db['mysql_host']
+app.config['MYSQL_PORT'] = db['mysql_port']
 app.config['MYSQL_USER'] = db['mysql_user']
 app.config['MYSQL_PASSWORD'] = db['mysql_password']
 app.config['MYSQL_DB'] = db['mysql_db']
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
 mysql = MySQL(app)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM PROJETOS")
+    results = cur.fetchall()
+    print(results[0]['DESCRICAO'])
+    return render_template('index.html', results = results)
+
+@app.route('/criar', methods=['GET', 'POST'])
+def criar():
     if request.method == 'POST':
-        # Fetch form data
-        userDetails = request.form
-        name = userDetails['name']
-        email = userDetails['email']
+        proj_salario = request.form['salario']
+        proj_titulo = request.form['titulo']
+        proj_endereco = request.form['endereco']
+        proj_contato = request.form['contato']
+        proj_desc = request.form['descricao']
         cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO users(name, email) VALUES(%s, %s)",(name, email))
+        cur.execute("INSERT INTO PROJETOS (ID_EMPRESA, TITULO, CONTATO, SALARIO, ENDERECO, DESCRICAO, IMAGEM) VALUES (1, %s, %s, %s, %s, %s, %s)", (proj_titulo, proj_contato, proj_salario, proj_endereco, proj_desc, "img a add"))
         mysql.connection.commit()
         cur.close()
-        return redirect('/users')
-    return render_template('index.html')
-
-@app.route('/users')
-def users():
-    cur = mysql.connection.cursor()
-    resultValue = cur.execute("SELECT * FROM users")
-    if resultValue > 0:
-        userDetails = cur.fetchall()
-        return render_template('users.html',userDetails=userDetails)
+    return render_template('criar.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
